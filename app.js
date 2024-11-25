78af4469f54b0538a549a2cb7eaa8f70b7141df7
Rengkat <successinmotion11@gmail.com>
2024-11-25 21:25:37 +0100

    app setup

---
+++ app.js
@@ -0,0 +1,27 @@
+//load .env
+
+//express
+const express = require("express");
+const app = express();
+
+// inbuilt modules
+
+// rest of packages
+
+// data base
+
+// route importations
+
+// middleware importations
+
+// middleware initialization
+
+// home route for documentation
+
+// route initialization
+
+// error initialization
+
+// starting the app
+const port = 5000;
+app.listen(port, () => console.log(`Server running on port ${port}...`));
