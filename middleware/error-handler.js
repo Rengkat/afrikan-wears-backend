c24292d2bbec51d123c8f9d476bf04479ab76b2f
Rengkat <successinmotion11@gmail.com>
2024-11-27 15:46:09 +0100

    custom error completed

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -7,8 +7,25 @@ const errorHandlerMiddleware = (err, req, res, nex) => {
     message: err.message || "Something went wrong, try again later",
   };
   //mongoose validation error
+  if (err.name === "ValidationError") {
+    customError.message = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(", ");
+  }
+  customError.statusCode = StatusCodes.BAD_REQUEST;
   //mongoose duplicate error
-  //mongoose cast error (invalid _d format)
+  if (err.code && err.code === 11000) {
+    customError.message = `Duplicate value entered for ${Object.keys(
+      err.keyValue
+    )} field, please choose another value.`;
+    customError.statusCode = StatusCodes.BAD_REQUEST;
+  }
+  //mongoose cast error (invalid _id format)
+  if (err.name === "CastError") {
+    customError.message = `Invalid ${err.path}: ${err.value}.`;
+    customError.statusCode = StatusCodes.NOT_FOUND;
+  }
+
   return res.status(customError.statusCode).json({ message: customError.message, success: false });
 };
 module.exports = errorHandlerMiddleware;
