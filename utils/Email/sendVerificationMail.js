0075a6c60971983f45b67e2fdf53758eb76415e5
unknown <successinmotion11@gmail.com>
2025-03-08 05:09:39 +0100

    send verification mail added

--- utils/Email/sendVerificationMail.js
+++ utils/Email/sendVerificationMail.js
@@ -1,1 +1,8 @@
-const sendVerificationEmail = async () => {};
+const sendEmail = require("./sendMail");
+
+const sendVerificationEmail = async ({ email, origin, name, verificationToken }) => {
+  const link = `${origin}/auth/verify-email?verificationToken=${verificationToken}&email=${email}`;
+  const html = ``;
+  sendEmail({ to: email, html, subject: "Email Verification" });
+};
+module.exports = sendVerificationEmail;
