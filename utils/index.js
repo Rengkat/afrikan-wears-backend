6c5ec1c41ae166d8a4b9a437c6847101b75ed6fe
Rengkat <successinmotion11@gmail.com>
2025-03-10 11:51:59 +0100

    verify token added

--- utils/index.js
+++ utils/index.js
@@ -1,5 +1,8 @@
+const { attachTokenToResponse, isTokenVerified } = require("./jwt");
 const createUserPayload = require("./userPayload");
 
 module.exports = {
   createUserPayload,
+  attachTokenToResponse,
+  isTokenVerified,
 };
--- utils/jwt.js
+++ utils/jwt.js
@@ -3,8 +3,22 @@ const createJwt = ({ payload }) => {
   const token = jwt.sign(payload, process.env.JWT_SECRET);
   return token;
 };
+const isTokenVerified = (token) => jwt.verify(token, process.env.JWT_SECRET);
 const attachTokenToResponse = ({ res, userPayload, refreshToken }) => {
   const accessTokenJWT = createJwt({ payload: { accessToken: userPayload } });
   const refreshTokenJWT = createJwt({ payload: { accessToken: userPayload, refreshToken } });
   //attach to cookies
+  res.cookie("accessToken", accessTokenJWT, {
+    httpOnly: true,
+    maxAge: 1000 * 60 * 5,
+    signed: true,
+    secure: false,
+  });
+  res.cookie("refreshToken", refreshToken, {
+    httpOnly: true,
+    expires: new Date(Date.now()) + 100 * 60 * 60 * 24 * 30,
+    signed: true,
+    secure: false,
+  });
 };
+module.exports = { attachTokenToResponse, isTokenVerified };
