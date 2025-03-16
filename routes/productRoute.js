f021fd966a16021e3995b1f7ecbd9b06637d2ecd
Rengkat <successinmotion11@gmail.com>
2025-03-16 02:44:29 +0100

    product router added

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -4,10 +4,21 @@ const {
   adminAndStylistAuthorization,
   stylistAuthorization,
 } = require("../middleware/authentication");
-const { addProduct, getAllProducts } = require("../controllers/productController");
+const {
+  addProduct,
+  getAllProducts,
+  getDetailProduct,
+  updateProduct,
+  deleteProduct,
+} = require("../controllers/productController");
 const router = express.Router();
 router
   .route("/")
-  .post(adminAndStylistAuthorization("stylist", "admin"), addProduct)
+  .post(authenticateUser, adminAndStylistAuthorization("stylist", "admin"), addProduct)
   .get(getAllProducts);
+router
+  .route("/productId")
+  .get(getDetailProduct)
+  .patch(authenticateUser, adminAndStylistAuthorization("stylist", "admin"), updateProduct)
+  .delete(authenticateUser, adminAndStylistAuthorization("stylist", "admin"), deleteProduct);
 module.exports = router;
