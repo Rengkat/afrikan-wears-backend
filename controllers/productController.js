07a37a066793ed75e2e177c1a6520acef57e3dbd
Rengkat <successinmotion11@gmail.com>
2025-03-16 11:32:25 +0100

    update product controller added

--- controllers/productController.js
+++ controllers/productController.js
@@ -62,6 +62,34 @@ const getDetailProduct = async (req, res, next) => {
     next(error);
   }
 };
-const updateProduct = async (req, res, next) => {};
+const updateProduct = async (req, res, next) => {
+  try {
+    const { productId: id } = req.params;
+    const { name, price, image, brand, sku, description, category, stock, rating, featured } =
+      req.body;
+
+    const product = await Product.findById(id);
+    if (!product) {
+      throw new CustomError.NotFoundError(`Product with ID ${id} not found`);
+    }
+
+    product.name = name || product.name;
+    product.price = price || product.price;
+    product.image = image || product.image;
+    product.brand = brand || product.brand;
+    product.sku = sku || product.sku;
+    product.description = description || product.description;
+    product.category = category || product.category;
+    product.stock = stock || product.stock;
+    product.rating = rating || product.rating;
+    product.featured = featured || product.featured;
+
+    await product.save();
+
+    res.status(200).json({ success: true, product, message: "product updated" });
+  } catch (error) {
+    next(error);
+  }
+};
 const deleteProduct = async (req, res, next) => {};
 module.exports = { addProduct, getAllProducts, getDetailProduct, updateProduct, deleteProduct };
