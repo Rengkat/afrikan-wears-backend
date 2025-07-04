0a35922f945300a51dde6d18dfc4114a1c6f6991
Rengkat <successinmotion11@.com>
2025-07-04 02:55:16 +0100

    wishlist contrroute

---
+++ routes/wishlistRoute.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+const wishlistController = require("../controllers/wishlistController");
+const { authenticateUser, restrictToUser } = require("../middleware/authentication");
+
+router.use(authenticateUser);
+
+router
+  .route("/")
+  .post(authenticateUser, restrictToUser("user"), addToWishlist)
+  .get(authenticateUser, restrictToUser("user"), getMyWishlist);
+
+router.route("/:productId").delete(authenticateUser, restrictToUser("user"), removeFromWishlist);
+
+router.route("/move-to-cart").post(authenticateUser, restrictToUser("user"), moveToCart);
+
+module.exports = router;
